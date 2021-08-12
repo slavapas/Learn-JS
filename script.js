6866acd8887896aaf1da1fe332b6bc9d6e4bed55
Slava Pascaru <pascaruv@gmail.com>
2021-08-12 16:17:11 +0300

    2nd commit

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // add div element into HTML
 let newLi = document.createElement('li')
+// add a class to newLi
+newLi.classList.add('my-li-class')
 // add div element into HTML
 let newA = document.createElement('a')
 // get menu
 let mainMenu = document.getElementById('main-nav').getElementsByTagName('ul')[0]
 
-// add our new element
-mainMenu.appendChild(newLi).appendChild(newA)
+// add our new element inside mainMenu
+mainMenu.appendChild(newLi)
 mainMenu.appendChild(newA)
 
 
